64246968a2cd688f91352e59dfa451da6e0a2b7e
BestStepInUa <beststep.in.ua@gmail.com>
2023-10-25 02:37:49 +0300

    start work on onSearchFormSubmit

---
+++ src/js/fetch-imgs.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default async function fetchImgs(search, page) {
+    const BASE_API_URL = 'https://pixabay.com/api/';
+    const API_KEY = '40254298-f0122bcc19424dfed523c5016';
+
+    const params = new URLSearchParams({
+
+        key: API_KEY,
+        q: `${search}`,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page: `${page}`,
+    });
+
+    axios.get(`${BASE_API_URL}?${params}`)
+        .then(resp => resp.data);
+};
---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+import fetchImgs from "./fetch-imgs";
+
+fetchImgs('dog', 1);
+
+const Refs = {
+    searchForm: document.querySelector('.search-form'),
+    imgsGallery: document.querySelector('.gallery'),
+    loadMoreBt: document.querySelector('.load-more')
+}
+
+Refs.searchForm.addEventListener('submit', onSearchFormSubmit);
+
+async function onSubmitSearchForm(evt) {
+    evt.preventDefault();
+};
